5df1e8c13ef90ec91e0035be292bf8557e56f9c0
tyjharrison2-art <tyj.harrison2@gmail.com>
2026-04-12 20:24:00 -0400

    Add simple math quiz functionality

---
+++ script.js
@@ -0,0 +1,25 @@
+let score = 0;
+let currentAnswer;
+
+function newQuest() {
+  let a = Math.floor(Math.random() * 10);
+  let b = Math.floor(Math.random() * 10);
+
+  currentAnswer = a + b;
+
+  document.getElementById("question").innerText = '${a} + ${b}';
+}
+
+function checkAnswer() {
+  let user = Number(document.getElementById("answer").value);
+  if (user === currentAnswer) {
+    score = score + 1;
+  }
+
+  document.getElementById("score").innerText = "Score: " + score;
+
+  document.getElementById("answer").value = "";
+  newQuest();
+}
+
+newQuest();
